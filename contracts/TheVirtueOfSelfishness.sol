cc926e695a6b9af9762faa68d136b75f09c33848
dzgoldman <dzgoldman@wesleyan.edu>
2021-07-10 10:13:54 -0400

    first commit

---
+++ contracts/TheVirtueOfSelfishness.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+
+contract TheVirtueOfSelfishness {
+  
+  mapping(uint => address) blockNumberToCoinbase;
+
+  fallback() external payable {}
+
+
+  function recordCoinbase() external {
+    blockNumberToCoinbase[block.number] = block.coinbase;
+  }
+
+  function claimSelfishPrize() external {
+    require(address(this).balance > 0, "NOTHING_TO_CLAIM_HERE" );
+    uint blocksMined = 1;
+    uint blockNumber = block.number -1;
+    while ( blockNumberToCoinbase[blockNumber] == block.coinbase ){
+      blocksMined ++;
+      blockNumber--;
+    }
+    require(blocksMined > 1, "NOT_SELFISH_ENOUGH");
+    block.coinbase.transfer( (address(this).balance * blocksMined)/(blocksMined + 1)  );
+
+  }
+
+
+}
